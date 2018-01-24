a8a23447f4b709be6f0eae775df7656d33e4b6b8
TeapoyYinYue <605528224@qq.com>
2018-01-24 17:58:09 +0800

    Initial
    script structure construction

---
+++ Assets/Witch/Script/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : Actor {
+    private int health;   //HP
+    private int mana;     //MP
+    private Vector3 location;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Witch/Script/GameMgr.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Witch
+{
+    public class GameMgr : MonoBehaviour
+    {
+        public static GameMgr instance = null;
+
+        //managers, attached on the gameMgr object
+        private SceneMgr sceneManager;
+        private ShaderMgr shaderManager;
+        private MessageMgr messageManager;
+        private ItemMgr ItemManager;
+        
+            
+        //  --- Unity default ---
+        // Use this for initialization
+        void Awake()
+        {
+            if (null == instance)
+            {
+                instance = this;
+            }
+            else if (this != instance)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+            
+            InitGame();
+            //get instance of managers
+            sceneManager = GetComponent<SceneMgr>();
+            shaderManager = GetComponent<ShaderMgr>();
+            messageManager = GetComponent<MessageMgr>();
+            ItemManager = GetComponent<ItemMgr>();
+        }
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        //  --- End of Unity default ---
+
+
+        //initiate game attributes, load data if necessary
+        void InitGame()
+        {
+            // to be implemented
+        }
+
+
+
+        
+    }
+
+
+
+}
